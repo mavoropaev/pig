683d0d6acbc314ec8bd90a4a20e8c7edde84c756
Vladimir Utratenko <vladimir.utratenko@sravni.ru>
2021-05-24 18:29:32 +0300

    Fix README and add logging

--- internal/http.go
+++ internal/http.go
@@ -12,15 +12,26 @@ func RunServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/internal/healthz", healthcheck)
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("resources/"))))
+	r.Use(loggingMiddleware)
 	http.Handle("/", r)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal().Err(err).Msg("Startup failed")
 	}
 }
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Debug().Msg(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func healthcheck(w http.ResponseWriter, r *http.Request) {
-	log.Debug().Msg("Triggering healthcheck")
 	var response API
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Server", "PIG Dummy Service")
 	if r.Method != http.MethodGet {
 		response = API{
 			Code:    405,
@@ -40,7 +51,5 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Add("Content-Type", "application/json")
-	w.Header().Set("Server", "PIG Dummy Service")
 	w.Write(js)
 }
